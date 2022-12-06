140891ebe4c9704658b346036d075cf54625df65
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-12-06 16:36:43 +0300

    add base url call to retrofit object

--- app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
+++ app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
@@ -19,7 +19,6 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.create
 import retrofit2.http.GET
 
 // DONE: Create a property for the base URL provided in the codelab
@@ -33,6 +32,7 @@ private val moshi = Moshi.Builder()
 // DONE: Build a Retrofit object with the Moshi converter
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
     .build()
 
 interface AmphibianApiService {
